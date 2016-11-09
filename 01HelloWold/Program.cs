4357bcbf8233351d654d604a95ce4cfdd433e6d2
devBjoern <kbnygaard@gmail.com>
2016-11-09 10:44:55 +0100

    Merged

---
+++ 01HelloWold/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// First funny program, made by Klaus Cramer Bjørn
+/// Doing funny things
+/// </summary>
+namespace _01HelloWold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn:");
+            string nameInput;
+            nameInput = Console.ReadLine();
+            nameInput = nameInput.ToUpper();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //To print
+            Console.WriteLine("Velkommen {0}", nameInput);
+            Console.WriteLine("Tryk en tast for at fortsætte...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02Variables/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _02Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNumber = 10;
+            Console.WriteLine("intNumber = " + intNumber);
+            intNumber++;
+            Console.WriteLine("intNumber = " + intNumber);
+            intNumber--;
+            Console.WriteLine("intNumber = " + intNumber);
+            intNumber += 20;
+            Console.WriteLine("intNumber = " + intNumber);
+
+
+            double doubleNumber = 12.5;
+            Console.WriteLine("doubleNumber = " + doubleNumber);
+            doubleNumber++;
+            Console.WriteLine("doubleNumber = " + doubleNumber);
+            doubleNumber--;
+            Console.WriteLine("doubleNumber = " + doubleNumber);
+            doubleNumber *= 2;
+            Console.WriteLine("doubleNumber = " + doubleNumber);
+
+            FilTyper ft = FilTyper.csv;
+            Console.WriteLine("Printing the ft text = " + ft);
+            int ftInt = (int)FilTyper.csv;
+            Console.WriteLine("Printing the ft value = " + ftInt);
+
+            DateTime myTime = DateTime.Now;
+            Console.WriteLine("myTime now = " + myTime.ToString("ddMMyy"));
+            Console.WriteLine("myTime now = " + myTime.ToString("dd. MMMM, yyyy"));
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+
+            Console.WriteLine("Personen p's navn er nu: " + p.Navn);
+
+
+            Console.ReadKey();
+        }
+            struct Person {
+            public int Id;
+            public string Navn;
+        }       
+
+
+            enum FilTyper {csv, pdf, txt};
+        
+    }
+}
