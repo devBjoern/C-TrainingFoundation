296bc3da6fead484629dfdd9fa5c55a01e34629e
devBjoern <kbnygaard@gmail.com>
2016-11-09 13:45:14 +0100

    Flere øvelser

--- 04RefType/04RefType.cs
+++ 04RefType/04RefType.cs
@@ -29,13 +29,59 @@ namespace _04RefType
             del = samletNavn.Substring(14);
             Console.WriteLine("delNavn: " + del);
 
-            string[] navnArray = { fornavn,efternavn};
+            string testSplit;
+            testSplit = fornavn + "," + efternavn;
+
+            string[] navnArray = testSplit.Split(',');
 
             foreach (var item in navnArray)
             {
                 Console.WriteLine(item);
             }
 
+            // Int array med variable månedslønninger
+
+            int[] monthSalaries = new int[7];
+            Random rndSalary = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                monthSalaries[i] = rndSalary.Next(40000, 100000);
+                Console.WriteLine("Salary " + i + " :" + monthSalaries[i]);
+            }
+
+            double avgSalary = Math.Round(monthSalaries.Average(),2);
+            
+            Console.WriteLine("Average salary is: " + avgSalary);
+
+            Array.Sort(monthSalaries);
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Salary " + i + " :" + monthSalaries[i]);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Her kommer den svedigste tabel:");
+            for (int xAkse = 1; xAkse < 11; xAkse++)
+            {
+                
+                for (int yAkse = 1; yAkse < 11; yAkse++)
+                {
+                    int temp = yAkse * xAkse;
+                    if (temp > 49)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string tal = temp.ToString();
+                    Console.Write(tal.PadLeft(4));
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
