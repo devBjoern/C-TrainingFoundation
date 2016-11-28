5511bc726bc54008c9606d00c6519cc8d34bd5cf
Klaus Cramer Bjørn <kbnygaard@gmail.com>
2016-11-28 16:11:08 +0100

    Props

---
+++ 07Prop/07Prop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace _07Prop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Areal af firkant med l og b = 10:");
+            Console.WriteLine(Arealberegninger.BeregnArealFirkant(10,10));
+            Console.WriteLine("Areal af cirkel med radius 10:");
+            Console.WriteLine(Arealberegninger.BeregnArealCirkel(10));
+            
+        }
+    }
+
+    public static class Arealberegninger
+    {
+        public static double BeregnArealFirkant(double højde, double bredde)
+        {
+            return højde * bredde;
+        }
+
+        public static double BeregnArealCirkel(double radius)
+        {
+            return Math.Round(Math.Pow(radius, 2) * Math.PI,3);
+        }
+    }
+}
---
+++ 07Prop02/07Prop02.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Prop02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person accountant = new Person();
+            accountant.Fornavn = "Brian";
+            accountant.Efternavn = "Jespersen";
+
+            Person trader = new Person();
+            trader.Fornavn = "Poul";
+            trader.Efternavn = "Ib";
+
+            //Console.WriteLine(accountant.Fuldtnavn());
+            //Console.WriteLine(trader.Fuldtnavn());
+            Console.WriteLine(accountant.FuldtNavn);
+            Console.WriteLine(trader.FuldtNavn);
+
+        }
+    }
+
+    class Person
+    {
+        public string Fornavn { get; set; }
+
+        private string efternavn;
+
+        public string Efternavn         
+        {
+            get { return efternavn; }
+            set {
+                if(value.Length <= 3)
+                {
+                    efternavn = "";
+                }
+                else
+                {
+                    efternavn = value;
+                }
+
+            }
+        }
+
+        //public string Fuldtnavn()
+        //{
+        //    return Fornavn + " " + Efternavn;
+        //}
+
+        private string fuldtNavn;
+
+        public string FuldtNavn
+        {
+            get {
+                fuldtNavn = Fornavn + " " + Efternavn;
+                return fuldtNavn; }
+        }
+   
+    }
+}
