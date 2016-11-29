a179bbb2fecb98eaae8f826babfb77b701ac567f
Klaus Cramer Bjørn <kbnygaard@gmail.com>
2016-11-29 15:39:38 +0100

    Exeptions

---
+++ 09Exception/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+    
+    private static void Run()
+        {
+            Console.WriteLine("Indtast 1. tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+        int tal2 = Convert.ToInt32(Console.ReadLine());
+        int res = tal1 + tal2;
+        Console.WriteLine("resultatet er "+res);
+        }
+    }
+}
